86187b90ffe9bfa0d8375fd97b5581d83975a5df
Yohannes251 <yohannes.z251@gmail.com>
2021-09-07 10:36:26 +0000

    Fixed condition for negative numbers

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -4,7 +4,10 @@ number = random.randint(-10000, 10000)
 numstr = str(number)
 num = int(numstr[len(numstr) - 1])
 string = "Last digit of {0} is {1} and is "
-if num > 5:
+_string = "Last digit of {0} is -{1} and is "
+if number < 0:
+    print(_string.format(number, num) + "less than 6 and not 0")
+elif num > 5:
     print(string.format(number, num) + "greater than 5")
 elif num == 0:
     print(string.format(number, num) + "0")
