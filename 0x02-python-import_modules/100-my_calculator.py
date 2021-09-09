c7a788f1fce5e8504f453a69b40198370c8e0e90
Yohannes251 <yohannes.z251@gmail.com>
2021-09-09 21:18:19 +0000

    Some module changes

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -8,26 +8,25 @@ def main():
     if argc != 4:
         print("Usage: {:s} <a> <operator> <b>".format(sys.argv[0]))
         sys.exit(1)
-    elif sys.argv[2] not in ["+", "-", "*", "/"]:
+    if sys.argv[2] not in ["+", "-", "*", "/"]:
         print("Unknown operator. Available operators: +, -, * and /")
         sys.exit(1)
-    else:
-        a = int(sys.argv[1])
-        b = int(sys.argv[3])
-        operator = sys.argv[2]
-        if operator == "+":
-            result = add(a, b)
-            print("{:d} {:s} {:d} = {:d}".format(a, operator, b, result))
-        elif operator == "-":
-            result = sub(a, b)
-            print("{:d} {:s} {:d} = {:d}".format(a, operator, b, result))
-        elif operator == "*" or operator[0] == "*":
-            result = mul(a, b)
-            print("{:d} {:s} {:d} = {:d}".format(a, operator, b, result))
-        elif operator == "/":
-            result = div(a, b)
-            print("{:d} {:s} {:d} = {:d}".format(a, operator, b, result))
+    a = int(sys.argv[1])
+    b = int(sys.argv[3])
+    operator = sys.argv[2]
+    if operator == "+":
+        result = calculator_1.add(a, b)
+        print("{:d} {:s} {:d} = {:d}".format(a, operator, b, result))
+    elif operator == "-":
+        result = calculator_1.sub(a, b)
+        print("{:d} {:s} {:d} = {:d}".format(a, operator, b, result))
+    elif operator == "*" or operator[0] == "*":
+        result = calculator_1.mul(a, b)
+        print("{:d} {:s} {:d} = {:d}".format(a, operator, b, result))
+    elif operator == "/":
+        result = calculator_1.div(a, b)
+        print("{:d} {:s} {:d} = {:d}".format(a, operator, b, result))
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
