234a30f083ae1ea20b44c2dab5c4fd36aae4b650
Yohannes251 <yohannes.z251@gmail.com>
2021-09-09 19:59:08 +0000

    Added sys extention to exit

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -7,10 +7,10 @@ def main():
     argc = len(sys.argv)
     if argc != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-        exit(1)
+        sys.exit(1)
     elif sys.argv[2] not in ["+", "-", "*", "/"]:
         print("Unknown operator. Available operators: +, -, * and /")
-        exit(1)
+        sys.exit(1)
     else:
         a = int(sys.argv[1])
         b = int(sys.argv[3])
