2bd912e3f38726b9b4af0cccb6ca01e506052631
Yohannes251 <yohannes.z251@gmail.com>
2021-10-05 12:17:12 +0000

    Added coma and changed increment approach

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-def magic_string():
-    magic_string.counter += 1 ; return "BestSchool" * magic_string.counter
-magic_string.counter = 0
+def magic_string(c=[0]):
+    c[0] += 1
+    return ("BestSchool, " * c[0])[:-2]
