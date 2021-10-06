1be1d963a0a257f733c6a99da9cb8994225e4a5a
Yohannes251 <yohannes.z251@gmail.com>
2021-10-06 09:37:14 +0000

    Updated docstring and removed repeated value checker

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,34 +1,28 @@
 #!/usr/bin/python3
-""" creates a square with size"""
+"""
+This module defines a class called Square
+"""
 
 
 class Square:
-    """ create class
-    Attributes:
-        size: size of square
-    """
+    """This class implements a Square"""
 
     def __init__(self, size=0):
-        if not isinstance(size, int):
-            raise TypeError('size must be an integer')
-        elif size < 0:
-            raise ValueError('size must be >= 0')
+        """Intializes attributes"""
         self.__size = size
-
-    def area(self):
-        """ return area """
-        A = self.__size * self.__size
-        return (A)
 
     @property
     def size(self):
-        """ return def """
         return self.__size
 
     @size.setter
     def size(self, value):
-        if not isinstance(value, int):
+        if isinstance(value, int) is not True:
             raise TypeError("size must be an integer")
         elif value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
+
+    def area(self):
+        """Returns area of a square"""
+        return self.__size**2
