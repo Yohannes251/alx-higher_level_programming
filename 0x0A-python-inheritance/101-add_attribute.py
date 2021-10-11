c878a363517e98191d3753467b3af9e469d37919
Yohannes251 <yohannes.z251@gmail.com>
2021-10-11 19:22:53 +0000

    Used attribute setter

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -5,7 +5,9 @@ This module creates a function that adds a new attribute
 
 
 def add_attribute(self, name, value):
-    if isinstance(self, type):
-        self.name = value
-    else:
+    """Adds a new attribute to object if possible"""
+    if not hasattr(self, "__dict__") and not hasattr(self, "__slots__"):
         raise TypeError("can't add new attribute")
+    if not hasattr(self, name) and hasattr(self, "__slots__"):
+        raise TypeError("can't add new attribute")
+    setattr(self, name, value)
