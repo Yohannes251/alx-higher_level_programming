231540c795243c5fbcc992f506efb6cefecd839c
Yohannes251 <yohannes.z251@gmail.com>
2021-10-11 18:29:18 +0000

    Updated conditions

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -7,7 +7,6 @@ This module creates a function that compares an object with a class
 def inherits_from(obj, a_class):
     """Checks if an object is instance of a class inherited from given class"""
     if a_class != type(obj):
-        if issubclass(type(obj), a_class):
-            return True
-        else:
-            return False
+        return issubclass(type(obj), a_class)
+    else:
+        return False
