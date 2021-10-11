774325050561ba51b80b3ab61e85ca7e5e842c67
Yohannes251 <yohannes.z251@gmail.com>
2021-10-11 15:32:07 +0000

    Added a value validator function

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+This module creates an empty class
+"""
+
+
+class BaseGeometry:
+    """Contains an area function"""
+    def area(self):
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """Validates value"""
+        self.name = name
+        if type(value) is not int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
+        self.value = value
+        return self.value
