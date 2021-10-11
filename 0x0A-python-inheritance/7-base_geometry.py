e86371d19af687ba42c7a1b9d93f8a00b994739e
Yohannes251 <yohannes.z251@gmail.com>
2021-10-11 15:51:07 +0000

    Fixed BaseGeometry class and created a new sub-class Rectangle

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -11,10 +11,7 @@ class BaseGeometry:
 
     def integer_validator(self, name, value):
         """Validates value"""
-        self.name = name
         if type(value) is not int:
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
-        self.value = value
-        return self.value
---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+This module creates a class that inherits BaseGeometry class
+"""
+
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """Implements a rectangle"""
+    def __init__(self, width, height):
+        self.__width = super().integer_validator('width', width)
+        self.__height = super().integer_validator('height', height)
