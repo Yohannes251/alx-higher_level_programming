ed0b4b8b723506990d932c07c7bf46f39b0b106b
Yohannes251 <yohannes.z251@gmail.com>
2021-10-14 12:13:51 +0000

    Added a validator function that raises errors if found

--- 0x0C-python-almost_a_circle/README.md
+++ 0x0C-python-almost_a_circle/README.md
@@ -1,13 +1,24 @@
 # Tasks aimed at reviewing the following python concepts
-> Import modules
-> Exceptions
-> Class
-> Private attribute
-> Getter/setter
-> Class method
-> Static method
-> Inheritance
-> Unittest
-> Read/Write File
-> JSON Serialization/Deserialization
-> args and kwargs
+> **Import modules**
+>
+> **Exceptions**
+>
+> **Class**
+>
+> **Private attribute**
+>
+> **Getter/setter**
+>
+> **Class method**
+>
+> **Static method**
+>
+> **Inheritance**
+>
+> **Unittest**
+>
+> **Read/Write File**
+>
+> **JSON Serialization/Deserialization**
+>
+> **args and kwargs**
--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -7,37 +7,58 @@ from models.base import Base
 
 class Rectangle(Base):
     """Rectangle class"""
+
     def __init__(self, width, height, x=0, y=0, id=None):
+        """Initializes attributes"""
         self.width = width
         self.height = height
         self.x = x
         self.y = y
         super().__init__(id)
 
-        @property
-        def width(self):
-            return self.__width
-        @width.setter
-        def width(self, value):
-            self.__width = value
+    def validator(self, name, value, attr_type):
+        """Validates passed arguments"""
+        if type(value) is not int:
+            raise TypeError(f'{name} must be an integer')
+        if attr_type == 1:
+            if value <= 0:
+                raise ValueError(f'{name} must be > 0')
+        if attr_type == 2:
+            if value < 0:
+                raise ValueError(f'{name} must be >= 0')
 
-        @property
-        def height(self):
-            return self.__height
-        @height.setter
-        def height(self, value):
-            self.__height = value
+    @property
+    def width(self):
+        return self.__width
 
-        @property
-        def x(self):
-            return self.__x
-        @x.setter
-        def x(self, value):
-            self.__x = value
+    @width.setter
+    def width(self, value):
+        self.validator("width", value, 1)
+        self.__width = value
 
-        @property
-        def y(self):
-            return self.__y
-        @y.setter
-        def y(self, value):
-            self.__y = value
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        self.validator("height", value, 1)
+        self.__height = value
+
+    @property
+    def x(self):
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        self.validator("x", value, 2)
+        self.__x = value
+
+    @property
+    def y(self):
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        self.validator("y", value, 2)
+        self.__y = value
