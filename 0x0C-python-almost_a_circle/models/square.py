2dc441ad4005c8b29083c4432ccb5f1042f44dd7
Yohannes251 <yohannes.z251@gmail.com>
2021-10-16 10:15:24 +0000

    Added update function

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -21,7 +21,29 @@ class Square(Rectangle):
     def size(self, value):
         super().validator("width", value, 1)
         self._size = value
-        
+
+    def update(self, *args, **kwargs):
+        """Assigns an argument to each attribute"""
+        if args and args is not None:
+            for i in range(len(args)):
+                if i == 0:
+                    self.id = args[i]
+                elif i == 1:
+                    self.size = args[i]
+                elif i == 2:
+                    self.x = args[i]
+                elif i == 3:
+                    self.y = args[i]
+        else:
+            keys = kwargs.keys()
+            if 'id' in keys:
+                self.id = kwargs['id']
+            if 'size' in keys:
+                self.size = kwargs['size']
+            if 'x' in keys:
+                self.x = kwargs['x']
+            if 'y' in keys:
+                self.y = kwargs['y'] 
 
     def __str__(self):
         """Returns readable representation of object instance"""
