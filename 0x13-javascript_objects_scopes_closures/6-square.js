2914e434c1829101783f2838a7d3f771ab8003e8
Yohannes251 <yohannes.z251@gmail.com>
2021-12-14 21:53:57 +0000

    Inherits previous square

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+
+const SuperSquare = require('./5-square');
+
+module.exports = class Square extends SuperSquare {
+  charPrint (c) {
+    if (c === 'C') {
+      const x = this.width;
+      let y = this.height;
+      while (y > 0) {
+        console.log('C'.repeat(x));
+        y--;
+      }
+    } else {
+      return this.print();
+    }
+  }
+};
