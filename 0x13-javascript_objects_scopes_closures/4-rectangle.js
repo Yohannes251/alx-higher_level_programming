264669b870c54f3092d9f7daaa99e527a8b18775
Yohannes251 <yohannes.z251@gmail.com>
2021-12-14 21:19:24 +0000

    Added two instance methods

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,28 @@
+#!/usr/bin/node
+
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    const x = this.width;
+    let y = this.height;
+    while (y > 0) {
+      console.log('X'.repeat(x));
+      y--;
+    }
+  }
+
+  rotate () {
+    [this.width, this.height] = [this.height, this.width];
+  }
+
+  double () {
+    this.width *= 2;
+    this.height *= 2;
+  }
+};
