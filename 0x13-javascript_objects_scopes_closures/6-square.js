0e09b1f9859d09c2f489bbb41c04c072b0e0567e
Yohannes251 <yohannes.z251@gmail.com>
2021-12-14 22:04:08 +0000

    Updated variables

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -5,11 +5,11 @@ const SuperSquare = require('./5-square');
 module.exports = class Square extends SuperSquare {
   charPrint (c) {
     if (c !== undefined) {
-      const x = this.width;
-      let y = this.height;
-      while (y > 0) {
-        console.log('C'.repeat(x));
-        y--;
+      const i = this.width;
+      let j = this.height;
+      while (j > 0) {
+        console.log('C'.repeat(i));
+        j--;
       }
     } else {
       return this.print();
