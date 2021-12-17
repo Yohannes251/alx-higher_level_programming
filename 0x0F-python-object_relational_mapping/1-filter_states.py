f00390be89447ac3b950ad6317d9639edf8080e6
Yohannes251 <yohannes.z251@gmail.com>
2021-12-17 13:32:54 +0000

    Filter by first letter

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+    This script lists all states from the database hbtn_0e_0_usa
+"""
+
+
+from sys import argv
+import MySQLdb
+
+
+db = MySQLdb.connect("localhost", argv[1], argv[2], argv[3])
+cur = db.cursor()
+cur.execute("SELECT * FROM states WHERE name REGEXP '^[N]' ORDER BY states.id")
+for row in cur.fetchall():
+    print(row)
