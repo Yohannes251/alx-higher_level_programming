e9ae42ba1122cb8d30299337cdbf237160edbca9
Yohannes251 <yohannes.z251@gmail.com>
2021-12-18 16:13:39 +0000

    added import guard and updated connect parameters

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -3,13 +3,16 @@
     This script lists all states from the database hbtn_0e_0_usa
 """
 
-
 from sys import argv
 import MySQLdb
 
-
-db = MySQLdb.connect("localhost", argv[1], argv[2], argv[3])
-cur = db.cursor()
-cur.execute("SELECT * FROM states WHERE name REGEXP '^[N]' ORDER BY states.id")
-for row in cur.fetchall():
-    print(row)
+if __name__ == '__main__':
+    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                         password=argv[2], database=argv[3])
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE name REGEXP '^[N]' \
+                ORDER BY states.id")
+    for row in cur.fetchall():
+        print(row)
+    cur.close()
+    db.close()
