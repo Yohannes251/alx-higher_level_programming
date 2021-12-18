be036e02795746eec4f2862b4e0bec3435d6823b
Yohannes251 <yohannes.z251@gmail.com>
2021-12-18 16:01:32 +0000

    Added import guard

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -7,9 +7,13 @@
 from sys import argv
 import MySQLdb
 
+if __name__ == '__main__':
 
-db = MySQLdb.connect("localhost", argv[1], argv[2], argv[3])
-cur = db.cursor()
-cur.execute("SELECT * FROM states ORDER BY states.id")
-for row in cur.fetchall():
-    print(row)
+    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                         password=argv[2], database=argv[3])
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states ORDER BY states.id")
+    for row in cur.fetchall():
+        print(row)
+    cur.close()
+    db.close()
