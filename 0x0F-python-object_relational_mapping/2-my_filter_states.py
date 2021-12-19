c1db24f18d29632b781763a2d0b929a2bb6e9401
Yohannes251 <yohannes.z251@gmail.com>
2021-12-19 12:23:26 +0000

    Matching an argument

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+    This script lists all states from the database hbtn_0e_0_usa
+"""
+
+from sys import argv
+import MySQLdb
+
+if __name__ == '__main__':
+    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                         password=argv[2], database=argv[3])
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE name REGEXP '{:s}' \
+                ORDER BY states.id ASC".format(argv[4]))
+    for row in cur.fetchall():
+        print(row)
+    cur.close()
+    db.close()
